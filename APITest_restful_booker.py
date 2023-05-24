f58b366ddd59a1a0b618fe014325febfd3a260a8
AleksandraMaiurova <asmaiurova@gmail.com>
2023-05-24 12:02:54 +0300

    Updated the last test

--- APITest_restful_booker.py
+++ APITest_restful_booker.py
@@ -32,4 +32,51 @@ def test_booking(api_request_context: APIRequestContext) -> None:
     headers = {"Content-type": "application/json"}
     response = api_request_context.get(f"https://restful-booker.herokuapp.com/booking", headers=headers)
     assert response.status == 200
+    assert isinstance(response.json(), list)
+    print(response.json()[0]['bookingid'])
+    assert response.json()[0]['bookingid'] is not None
+
+@pytest.fixture(scope="session")
+def idbooking():
+    headers = {"Content-type": "application/json"}
+    response = requests.get(f"https://restful-booker.herokuapp.com/booking", headers=headers)
+    return response.json()[0]['bookingid']
+
+def test_GetBooking (idbooking, api_request_context: APIRequestContext) -> None:
+    headers = {"Content-type": "application/json"}
+    response = api_request_context.get(f'https://restful-booker.herokuapp.com/booking/{idbooking}', headers=headers)
+    assert response.status == 200
+    print(response.json())
+    assert isinstance(response.json(), dict)
+    assert response.json()["firstname"] == "John"
+    assert response.json()["lastname"] == "Smith"
+    assert response.json()["totalprice"] == 111
+
+def test_CreateBooking (api_request_context: APIRequestContext) -> None:
+    headers = {
+        "Content-type": "application/json",
+        "Accept": "application/json"}
+    data = {
+        "firstname": "Aleksandra",
+        "lastname": "Maiurova",
+        "totalprice": 112,
+        "depositpaid": True,
+        "bookingdates": {
+         "checkin": "2023-01-01",
+         "checkout": "2023-05-01"
+        },
+        "additionalneeds": "Breakfast for my cat"
+    }
+    response = api_request_context.post(f"https://restful-booker.herokuapp.com/booking", data=data, headers=headers)
+    assert response.status == 200
+    assert response.json()['bookingid'] is not None
+
+def test_DeleteBooking (auth, idbooking, api_request_context: APIRequestContext) -> None:
+    headers = {"Content-type": "application/json",
+               "Cookie": f'token={auth}'}
+    response = api_request_context.delete(f'https://restful-booker.herokuapp.com/booking/{idbooking}', headers=headers)
+    assert response.status == 201
+
+
+
 
